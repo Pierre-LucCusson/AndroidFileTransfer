cb066f22d4854f007309f6943555afe11acab932
Pierre-Luc Cusson <plcusson@gmail.com>
2018-07-05 20:18:10 -0400

    add basic client

---
+++ app/src/main/java/com/androidfiletransfer/Client.java
@@ -0,0 +1,30 @@
+package com.androidfiletransfer;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Client {
+
+    private OkHttpClient client;
+    private String serverUrl;
+
+    public Client(String serverUrl) {
+        this.serverUrl = "http://" + serverUrl;
+        client = new OkHttpClient();
+    }
+
+
+    public String run(String url) {
+
+        Request request = new Request.Builder()
+                .url(serverUrl + url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
