f04fb0ed5553fa31b87ea78a25be664a852b3d0e
Haedy <al_themafia@hotmail.fr>
2018-07-08 20:51:29 -0400

    Ajout d<une fonction pour recup la localisation

--- app/src/main/java/com/androidfiletransfer/Tracker.java
+++ app/src/main/java/com/androidfiletransfer/Tracker.java
@@ -3,9 +3,12 @@ package com.androidfiletransfer;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+
+import java.util.List;
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -23,17 +26,37 @@ public class Tracker {
         }
     }
 
-    public boolean startTracking(LocationListener locationListener)
-    {
+    public boolean startTracking(LocationListener locationListener) {
         boolean active = false;
 
-        if( this.activity != null ) {
-            if (ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (this.activity != null) {
+            if (ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, locationListener);
                 active = true;
             }
         }
 
         return active;
+    }
+
+    public Location getLastLocation() {
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+
+        if (ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            for (String provider : providers) {
+                Location l = locationManager.getLastKnownLocation(provider);
+                if (l == null) {
+                    continue;
+                }
+
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    bestLocation = l;
+                }
+            }
+
+        }
+
+        return bestLocation;
     }
 }
